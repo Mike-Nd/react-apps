6d104b106b894c78d4c7203a28bfb774e2935f4b
Michael James <mikesnet4life@gmail.com>
2022-12-20 14:11:25 +0100

    solved the delete bug

--- src/App.js
+++ src/App.js
@@ -2,29 +2,36 @@ import "./App.css";
 import Task from "./components/task";
 import React, { useState } from "react";
 
-
 function App() {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   const [list, setList] = useState([]);
 
+  const addTodo = () => {
+    //Add task
+    list.push({ description: task });
+    setList(list);
 
-  const addTodo = (todo) => {
-    const newTodo = {
-      id: Math.random(),
-      todo : todo
-    }
-    setList([...list, newTodo]);
-
+    //clear input box
     setTask("");
+  };
+
+  //delete tasks.
+  const deleteTodo = (id) => {
+    const newList = list.filter((listItem, i) => i != id);
+    setList(newList);
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="header-title">Today's task.</h1>
-
-        <div className="tasks">
-          <Task text={"Task 1"} />
+        <h1 className="app-title">TASKIT</h1>
+        <div>
+          {list.length ? (
+            list.map((item, index) =>
+              <Task id={index} text={item} deleteTodo={deleteTodo} />)
+          ) : (
+            <p className="taskAbsense">No task avalaible.</p>
+          )}
         </div>
       </header>
 
@@ -36,14 +43,16 @@ function App() {
           onChange={(e) => setTask(e.target.value)}
         />
 
-        <button
-          className="input-icon"
-          onClick={()=> addTodo(task) }>
+        <button className="input-icon" onClick={() => addTodo(task)}>
           +
         </button>
       </div>
     </div>
   );
 }
+
+//don't forget the eventListeners ; onChange, onClick, onInput and so on as the allow you know when to react.
+//also remember the map, filter, spread,splice and push function they give you more flexibility when dealing with arrays.
+//the state is one of the most important as it allows you have multiple states in your code, props are just movable codes and hooks allow you to manupulate the state of your program.
 
 export default App;
--- src/components/task.js
+++ src/components/task.js
@@ -1,18 +1,22 @@
-import './task.css';
+import "./task.css";
 
 function Task(props) {
-    return (
-        <div className="task-wrapper">
-            <div className="task-main">
-                <button className="square"></button>
-                <p className="task-text">{props.text}</p>
-            </div>
+  return (
+    <div className="task-wrapper">
+      <div
+        key={props.key}
+        className="task-main">
+        
+        <button className="square"/>
+        <p className="task-text">{props.text.description}</p>
+      </div>
 
-            <div className="circle"></div>
-        </div>
-    );
+      <div
+        className="circle"
+        onClick={() => props.deleteTodo(props.id)}
+      />
+    </div>
+  );
 }
 
 export default Task;
-
-
